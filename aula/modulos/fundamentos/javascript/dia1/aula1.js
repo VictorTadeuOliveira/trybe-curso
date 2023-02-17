87eb48122bc112dfbdc2dfebec4be68fb0ecba05
Victor Tadeu <vic.tad.oli@gmail.com>
2023-02-17 19:21:17 -0300

    Aula de JavaScript Dia1

---
+++ aula/modulos/fundamentos/javascript/dia1/aula1.js
@@ -0,0 +1,5 @@
+document.getElementById("page-title").innerText = "Forrest Gump";
+
+document.getElementById("second-paragraph").innerText = "Texto do segundo parágrafo";
+
+document.getElementById("subtitle").innerText = "Texto do subtítulo";
