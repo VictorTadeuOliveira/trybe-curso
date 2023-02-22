6c5cd80005fc00fe060de75be27bfd38f18d7653
Victor Tadeu <vic.tad.oli@gmail.com>
2023-02-22 15:58:30 -0300

    Conteúdo da aula do dia 4

---
+++ aula/modulos/fundamentos/javascript/dia4/aula1.js
@@ -0,0 +1,4 @@
+console.log(localStorage.length); // não possui nada salvo, então o retorno é o valor: 0
+localStorage.setItem('firstname', 'Adam'); // salva uma entrada com a key = 'firstname' e value = 'Adam'
+localStorage.setItem('lastname', 'Smith'); // salva uma entrada com a key = 'lastname' e value = 'Smith'
+console.log(localStorage);
